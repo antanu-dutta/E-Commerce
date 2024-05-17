7d24caa90bf9257cee81385ee5bbac725b7742e8
antanu-dutta <antanu.bittu@gmail.com>
2024-05-17 16:36:52 +0530

    e-commerce website

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [menu, setMenu] = useState("shop");
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={() => setMenu("shop")}>
+          <Link to="/">Shop</Link> {menu === "shop" && <hr />}
+        </li>
+        <li onClick={() => setMenu("mens")}>
+          <Link to="/mens">Men</Link> {menu === "mens" && <hr />}
+        </li>
+        <li onClick={() => setMenu("womens")}>
+          <Link to="/womens">Women</Link> {menu === "womens" && <hr />}
+        </li>
+        <li onClick={() => setMenu("kids")}>
+          <Link to="/kids">Kids</Link> {menu === "kids" && <hr />}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>
+          <Link style={{ textDecoration: "none" }} to="/login">
+            Login
+          </Link>
+        </button>
+        <Link to="/cart">
+          <img src={cart} alt="" />
+        </Link>
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
